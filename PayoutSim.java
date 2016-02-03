0035abcab3d125f06ccbc37b50290f15375b8145
Hetti <github@thepeko.at>
2016-02-03 14:19:10 +0100

    PayoutSimulator: init of repository

---
+++ PayoutSim.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class PayoutSim{
+
+	public static void main (String [] args) {
+		
+		System.out.println("PayoutSim initialised");
+		
+		String[] eventsValidator = {"{\"event\":\"fraud attempt\",\"dispensed\":%d}",
+			"{\"event\":\"read\",\"amount\":%d,\"channel\":%d}", 
+			"{\"event\":\"credit\",\"amount\":%d,\"channel\":%d}",
+			"{\"event\":\"incomplete payout\",\"dispensed\":%d,\"requested\":%d,\"cc\":\"%s\"}",
+			"{\"event\":\"incomplete float\",\"dispensed\":%d,\"requested\":%d,\"cc\":\"%s\"}",
+			"{\"event\":\"reading\"}",
+			"{\"event\":\"reading\"}",
+			"{\"event\":\"emptying\"}",
+			"{\"event\":\"rejecting\"}",
+			"{\"event\":\"rejected\"}",
+			"{\"event\":\"stacking\"}",
+			"{\"event\":\"stored\"}",
+			"{\"event\":\"stacked\"}",
+			"{\"event\":\"safe jam\"}",
+			"{\"event\":\"unsafe jam\"}",
+			"{\"event\":\"disabled\"}",
+			"{\"event\":\"stacker full\"}",
+			"{\"event\":\"cashbox removed\"}",
+			"{\"event\":\"cashbox replaced\"}",
+			"{\"event\":\"cleared from front\"}",
+			"{\"event\":\"cleared into cashbox\"}",
+			"{\"event\":\"calibration fail\",\"error\":\"no error\"}",
+			"{\"event\":\"calibration fail\",\"error\":\"sensor flap\"}",
+			"{\"event\":\"calibration fail\",\"error\":\"sensor exit\"}",
+			"{\"event\":\"calibration fail\",\"error\":\"sensor coil 1\"}",
+			"{\"event\":\"calibration fail\",\"error\":\"sensor coil 2\"}",
+			"{\"event\":\"calibration fail\",\"error\":\"not initialized\"}",
+			"{\"event\":\"calibration fail\",\"error\":\"checksum error\"}",
+			"{\"event\":\"recalibrating\"}"
+			};
+
+		String[] eventsHopper = {"event\":\"credit", "event\":\"coin credit"};
+		int amount = 0;
+		int channelid = 2;
+		int arrayID = 2;
+		int hardware = 0;
+		String out = "test";
+		
+		 // create a scanner so we can read the command-line input
+		Scanner scanner = new Scanner(System.in);
+		
+		// prompt for Hopper or Validator
+		System.out.print("Enter 1 for Validator Event or 2 for Hopper Event \n");
+
+		// set hardware
+		hardware = scanner.nextInt();
+
+		//  prompt for wished Event
+		System.out.print("Enter Event you wish\n2 is event credit\n");
+
+		// get wished Event as arrayID
+		arrayID = scanner.nextInt();
+
+		// prompt for wished amount
+		System.out.print("Enter amount in cent:\n");
+
+		// set amount
+		amount = scanner.nextInt();
+		
+		if(arrayID == 0){
+			System.out.printf(eventsValidator[arrayID]+"\n",amount);
+		}
+		else if(arrayID == 1 || arrayID == 2){
+			System.out.printf(eventsValidator[arrayID]+"\n",amount,channelid);
+		}
+		else if(arrayID == 3 || arrayID == 4){
+			System.out.printf(eventsValidator[arrayID]+"\n",amount,channelid, out);
+		}
+		else{
+			System.out.printf(eventsValidator[arrayID]+"\n");
+		}
+
+		
+		String output = "{"+eventsValidator[0]+",\"amount\":"+amount+",\"channel\":"+channelid+"}";
+		
+	//	System.out.printf(eventsValidator[arrayID]+"\n",amount,channelid);
+		
+		
+		
+		
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# SMART Payout Simulator
+
+This program simulates output of the [SMART Hopper][itl-hw-hopper]
+and [NV200 Banknote validator][itl-hw-validator] devices from Innovative Technology.
+Basis is the low-level [SSP protocol][itl-ssp]
+
+
+
+[itl-ssp]: http://innovative-technology.com/product-files/ssp-manuals/smart-payout-ssp-manual.pdf
+[itl-hw-hopper]: http://innovative-technology.com/products/products-main/210-smart-hopper
+[itl-hw-validator]: http://innovative-technology.com/products/products-main/90-nv200
