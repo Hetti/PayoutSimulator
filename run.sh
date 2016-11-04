d23e4e2d3e92c2c58a6201179e53d8b7d03a4600
sixtyeight <christian_sl@gmx.at>
2016-11-04 03:24:23 +0100

    runme.sh split into build.sh and run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn exec:java -Dexec.mainClass="at.metalab.payoutsim.PayoutSimMain"
--- runme.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mvn install exec:java -Dexec.mainClass="at.metalab.payoutsim.PayoutSimMain"
